0f7f54b9c4d57e8adace071b7a9cb98757442f12
wongethan199 <wongethan199@gmail.com>
2025-01-17 13:46:32 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import searoute as sr
+import geopy
+st.header("Carbon Emission Transshipment")
+"Carbon emission calculator for 3 ports"
+choice=st.text_input("Enter mode 1 for sea and 0 for air(Default)")
